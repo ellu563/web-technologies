54f8f951837c9a302bd0401b193dd5170f17c10e
Elina P <elina.partinen1@gmail.com>
2022-11-27 15:32:41 +0200

    webteknologiat muokkaus

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,8 +1,8 @@
 let defaultValue;
 let defaultValue2;
 
-// jQueryllä
-// saadaan sivun latauksen yhteydessä joku numero siihen esiin
+// Kaytetaan jQuery
+// Saadaan sivun latauksen yhteydessä numero esiin
 $(function () {
   // arvo 1 ja 10 väliltä
   defaultValue = Math.floor(Math.random() * 10) + 1;
@@ -18,7 +18,7 @@ window.onload = function () {
   document.getElementById("second-nr").value = defaultValue;
 }; */
 
-// jos painetaan + painiketta, kasvatetaan numeroa yhdellä, muussa tapauksessa vähennetään sitä yhdellä
+// Jos painetaan + painiketta, kasvatetaan numeroa yhdellä, muussa tapauksessa vähennetään sitä yhdellä
 function setFirstNr(type) {
   // let currentValue = document.getElementById('first-nr').value; // luetaan arvo ja asetetaan muuttujaan
   let currentValue = $('#first-nr').val();
@@ -32,7 +32,8 @@ function setFirstNr(type) {
   $('#first-nr').val(currentValue); // first-nr id:n omistavan elementin arvo on nyt currentValue
 }
 
-// sama toiselle palkille, jos painetaan + painiketta, kasvatetaan numeroa yhdellä, muussa tapauksessa vähennetään sitä yhdellä
+// Sama tehdään toiselle palkille, jos painetaan + painiketta, kasvatetaan numeroa yhdellä
+// Muussa tapauksessa vähennetään sitä yhdellä
 function setSecondNr(type) {
   let currentValue = $('#second-nr').val();
 
@@ -45,20 +46,20 @@ function setSecondNr(type) {
   $('#second-nr').val(currentValue);
 }
 
-// lukee molempien kenttien arvot ja selvittää mikä laskutoimitus valittu, suorittaa laksutoimitus, ja näyttää sen tulos
+// Lukee molempien kenttien arvot ja selvittää mikä laskutoimitus valittu, suorittaa laskutoimituksen, ja näytetään sen tulos
 function calculate() {
-  // mikä operation select pudotusvalinnan valinta on valittu
+  // Mikä operation select pudotusvalinnan valinta on valittu
   let selectedText = $('#operationSelect option:selected').text();
 
   let first = $('#first-nr').val();
   first = parseInt(first);
 
-  let second = $('#second-nr').val(); // luetaan second-nr html elemtin arvo
-  second = parseInt(second); // varmistetaan että se on kokonaisluku
+  let second = $('#second-nr').val(); // Luetaan second-nr html elemtin arvo
+  second = parseInt(second); // Varmistetaan että se on kokonaisluku
 
-  // jos text on "+" niin laske yhteen yms
+  // Jos text on "+" niin laske yhteen yms
   if (selectedText == '+') {
-    // laitetaan result-html elementtiin laskun tulos
+    // Laitetaan result-html elementtiin laskun tulos
     $('#result').val(first + second);
   }
   if (selectedText == '-') {
@@ -72,14 +73,14 @@ function calculate() {
   }
 }
 
-// tapahtumakäsittelijä jqueryllä
+// Tapahtumakäsittelijä jqueryllä
 // eli kun klikataan calc-buttonia suoritetaan calculate function sekä validateForm
 $('#calc-button').click(function () {
   calculate();
   validateForm();
 });
 
-// valioidaan että numerot ovat molemmissa boxeissa
+// Validoidaan että numerot ovat molemmissa boxeissa
 function validateForm() {
   let x = document.forms['myForm']['first-nr'].value;
   if (x == '') {
